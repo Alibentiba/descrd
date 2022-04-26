736c7d03702ad5682770fb5b297c0fad58b4b9be
unknown <bentibaali3@gmail.com>
2022-04-26 12:06:36 +0100

    add mesge componenet

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import Chatheader from './Chatheader'
 import './Chat.css'
+import { IoMdAddCircle } from 'react-icons/io';
+import { AiOutlineGif } from 'react-icons/ai';
+import { MdEmojiEmotions } from 'react-icons/md';
+import { FaGift } from 'react-icons/fa';
+import { Messege } from '../Messeg/Messege';
+
+
 
 const Chat = () => {
   return (
     <div className='Chat'>
     <Chatheader/>
+
+    <div className='Chat_msg'>
+      <Messege/>
+    </div>
+
+      <div className='Chat_input'>
+        <IoMdAddCircle className='Chat_icons'/>
+        <form action="">
+        <input type="text" placeholder={`Messege #Test`} />
+        <button className='Chat-buton' type='submit'  >Send Messge</button>
+        </form>
+        <div className='input_icons'>
+       <FaGift className='Chat_icons'/>
+       <AiOutlineGif className='Chat_icons'/>
+       <MdEmojiEmotions className='Chat_icons'/>
+     </div>
+     </div>
+
+
+
 
     </div>
   )
--- src/Chat/Chatheader.jsx
+++ src/Chat/Chatheader.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import './Chatheader.css'
 import { FaBell,FaMapMarker,FaUserFriends,FaSearch,FaLocationArrow,FaQuestionCircle } from "react-icons/fa";
-
+import { MdSend } from "react-icons/md";
+import { BiSearchAlt2} from "react-icons/bi";
 const Chatheader = () => {
   return (
     <div className='chatheader'>
 
      <div className='chatheader_left'>
-<h4>#youtb</h4>
+<h4>#youtub</h4>
     </div>
-
     <div className='chatheader_right'>
-    <FaBell/>
-    <FaMapMarker/>
-    <FaUserFriends/>
+    <FaBell className='chatheader_right_icon'/>
+    <FaMapMarker className='chatheader_right_icon sendicon'/>
+    <FaUserFriends className='chatheader_right_icon'/>
     <div className='Chat_search'>
-    <input type="text" placeholder='serch'>
-    <FaSearch className='search_icon'/></input>
-    <FaLocationArrow/>
+    <input type="text" placeholder='search'/>
+    <BiSearchAlt2  className='chatheader_right_icon'/>
     </div>
-    <FaQuestionCircle/>
+    <MdSend className='chatheader_right_icon'/>
+  
+    <FaQuestionCircle className='chatheader_right_icon'/>
     </div>
 
 
