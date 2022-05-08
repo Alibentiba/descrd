5aa16a260dea5efc5bb49c463282e3e4f5f41721
Alibentiba <99966047+Alibentiba@users.noreply.github.com>
2022-05-08 22:46:06 +0100

    css styling

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,13 +9,19 @@ import { collection,getDocs,getFirestore } from "firebase/firestore";
 const Home = ()=>{
 const db =getFirestore()
 const colRef= collection(db,'movies')
-getDocs(colRef).then((snap)=>{
-  // console.log("tout les doc sont La ",snap.docs)
-  let newdata=[]
-  snap.docs.forEach((doc)=>{newdata.push({...doc.data(),id:doc.id})})
 
-  console.log('ls donnes sont les suivent ',newdata)
-})
+
+useEffect(()=>{
+  getDocs(colRef).then((snap)=>{
+    let newdata=[]
+    snap.docs.forEach((doc)=>{newdata.push({...doc.data(),id:doc.id})})
+  var vu=[]
+    console.log('ls donnes sont les suivent ',newdata)
+    newdata.forEach((doc)=>{vu=[...vu,doc.title]})
+    console.log('les subtitle ',vu)})
+
+},[])
+
 
 
   return (
