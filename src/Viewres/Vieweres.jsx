a7866a864ff62f304541d38d2217ef0bd4d68ea2
Alibentiba <99966047+Alibentiba@users.noreply.github.com>
2022-05-11 23:21:47 +0100

    Add Video to Viewers

--- src/Viewres/Vieweres.jsx
+++ src/Viewres/Vieweres.jsx
@@ -5,7 +5,13 @@ import viewersdisney from '../images/viewers-disney.png'
 import viewersstarwars from "../images/viewers-starwars.png"
 import viewerspixar from "../images/viewers-pixar.png"
 import "./Viewers.css"
+import marvel from "../videos/1564676115-marvel.mp4"
 // import {useSelector } from 'react-redux'
+import disney from "../videos/1564674844-disney.mp4"
+import national from "../videos/1564674844-disney.mp4"
+import pixar from "../videos/1564676714-pixar.mp4"
+import star from "../videos/1608229455-star-wars.mp4"
+
 
 const Vieweres = () => {
 
@@ -13,22 +19,52 @@ const Vieweres = () => {
     <div className='Viewers'>
         <div className='Viewers-image'>
         <img src={viewersmarvel} alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source
+            src={marvel}
+            type="video/mp4"
+          />
+        </video>
+
         </div>
 
         <div className='Viewers-image'>
-        <img src={viewersnational} alt="" />    
+        <img src={viewersnational} alt="" />  
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source
+            src={national}
+            type="video/mp4"
+          />
+        </video>  
             </div>
 
         <div className='Viewers-image'>
         <img src={viewersdisney} alt="" />   
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source
+            src={disney}
+            type="video/mp4"
+          />
+        </video>
              </div>
 
         <div className='Viewers-image'>
-        <img src={viewersstarwars} alt="" />    
+        <img src={viewersstarwars} alt="" />  
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source src={star}type="video/mp4"/>
+        </video>  
             </div>
-            <div className='Viewers-image'>
-        <img src={viewerspixar} alt="" />    
-            </div>
+       <div className='Viewers-image'>
+        <img src={viewerspixar} alt="" />  
+        <video
+        muted={true}
+         autoPlay={true}
+         preLoad={true}
+         loop={true} playsInline={true}>
+          <source src={pixar}type="video/mp4"/>
+        </video>  
+       
+   </div>
 
 
 
